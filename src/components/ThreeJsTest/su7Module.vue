1ba6daba533c945dcb9953de5dc197841aca6970
wangchuang <heiha.space@gmail.com>
2024-05-07 12:50:49 +0800

    添加辅助坐标轴

--- src/components/ThreeJsTest/su7Module.vue
+++ src/components/ThreeJsTest/su7Module.vue
@@ -4,15 +4,16 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 const scene = new THREE.Scene(); // 场景
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000) // 透视相机
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000) // 透视相机
 camera.position.set(3, 3, 7);
 camera.lookAt(0, 0, 0)
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth * 0.75 , window.innerHeight * 0.75);
 document.body.appendChild(renderer.domElement);
 
 
-const loader = new GLTFLoader();
+
+const loader = new GLTFLoader(); // 模型加载器
 const loaderFn = (gltf) => {
     scene.add(gltf.scene);
 }
@@ -21,7 +22,9 @@ loader.load('/public/3DModels/su7/scene.gltf', loaderFn, undefined, (error) => {
 });
 
 const ambient = new THREE.AmbientLight('#FFFFFF');
+const axesHelper = new THREE.AxesHelper(5); // 辅助坐标系线
 scene.add(ambient)
+scene.add(axesHelper)
 const animate = () => {
     requestAnimationFrame(animate);
     renderer.render(scene, camera)
